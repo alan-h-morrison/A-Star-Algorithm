4ab3902278d08bf9f4a6764775191115a8ea9f03
alan-h-morrison <amorrison12312@gmail.com>
2020-10-27 00:24:56 +0000

    Add project files.

---
+++ 40400403/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _40400403
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
