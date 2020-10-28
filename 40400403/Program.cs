59ec7026d70ec00e534f3dacbd32b8fb7c356bc3
alan-h-morrison <amorrison12312@gmail.com>
2020-10-28 00:56:42 +0000

    added test file, .cav files are read from the command line

--- 40400403/Program.cs
+++ 40400403/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace _40400403
 {
@@ -6,8 +11,16 @@ namespace _40400403
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("test");
+            //read in caves
+            string fileName = args[0];
+            string filePath = "testfiles/generated30-1/";
+
+            string file = System.IO.File.ReadAllText(filePath + fileName + ".cav");
+
+            Console.WriteLine(file);
+
+            //TextWriter output = new StreamWriter("testfiles/" + fileName + ".csv");
+            //output.WriteLine(file);
         }
     }
 }
